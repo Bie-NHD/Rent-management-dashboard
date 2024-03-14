719997716d1e07d3c70a71374c831cb0ac6194e2
Ng Huong Duyen <huongduyen.work@gmail.com>
2024-03-14 08:41:02 +0700

    deps: add MUI and delete staff-portal

--- frontend-2/src/App.jsx
+++ frontend-2/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
-import "../App.css";
-function Home() {
-  const [count, setCount] = useState(0);
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -28,6 +29,7 @@ function Home() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
-export default Home;
+
+export default App
