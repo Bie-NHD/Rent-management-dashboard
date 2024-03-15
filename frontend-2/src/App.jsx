f007ed6525d406afeba6612b77c4686849293838
Ng Huong Duyen <huongduyen.work@gmail.com>
2024-03-15 22:14:46 +0700

    try!add:SideBar

--- frontend-2/src/App.jsx
+++ frontend-2/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import ImportScreen from './pages/ImportScreen'
+import SideBar from './shared/SideBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,7 +31,10 @@ function App() {
     //     Click on the Vite and React logos to learn more
     //   </p>
     // </>
+    <>
+    <SideBar/>
     <ImportScreen/>
+    </>
   );
 }
 
