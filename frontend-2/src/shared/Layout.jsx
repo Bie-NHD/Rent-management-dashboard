b02caa08c9a2dad817ba7beea343c3728a07bc86
Duyên Ng <huongduyen.work@gmail.com>
2024-03-20 12:42:40 +0700

    feat: add stylings in AllApartmentsPage

--- frontend-2/src/shared/Layout.jsx
+++ frontend-2/src/shared/Layout.jsx
@@ -5,15 +5,22 @@ import SideBar from "./SideBar";
 const Layout = () => {
   return (
     <Container
-    sx={{
-      display: "flex",
-      minHeight: "100vh",
-      minWidth: "100vw",
-    }}
+      sx={{
+        display: "flex",
+        minHeight: "100vh",
+        minWidth: "100vw",
+        margin: 0,
+        padding: 0,
+      }}
     >
       <SideBar />
-      <Container>
-        <Outlet/>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Outlet />
       </Container>
     </Container>
   );
