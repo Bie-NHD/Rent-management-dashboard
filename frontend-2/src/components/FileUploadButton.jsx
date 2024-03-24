ef0bd30924a0aa33dbc53c581874edb712ce0ba3
Duyên Ng <huongduyen.work@gmail.com>
2024-03-24 22:56:24 +0700

    perf: change pj directory structure

--- frontend-2/src/components/FileUploadButton.jsx
+++ frontend-2/src/components/FileUploadButton.jsx
@@ -1,32 +1,32 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
+  overflow: "hidden",
+  position: "absolute",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
-const  InputFileUpload =()=> {
-    return (
-        <Button
-        component="label"
-        role={undefined}
-        variant="contained"
-        tabIndex={-1}
-        startIcon={<CloudUploadIcon />}
-        >
+const FileUploadButton = () => {
+  return (
+    <Button
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      startIcon={<CloudUploadIcon />}
+    >
       Upload file
       <VisuallyHiddenInput type="file" />
     </Button>
   );
-}
-export default FileUploadButton
+};
+export default FileUploadButton;
