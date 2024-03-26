65b53db64c70d03eb860dcb779106a2fc3d990db
Duyên Ng <huongduyen.work@gmail.com>
2024-03-26 22:53:50 +0700

    add: base API Requests

--- frontend-2/src/api/index.js
+++ frontend-2/src/api/index.js
@@ -2,9 +2,58 @@ import axios from "axios";
 
 import { testApartmentsOnly, testContractsOnly } from "./test";
 
-import { TEST_URL, API_ROUTE_APARMENT } from "../utils/constants";
+import {
+  TEST_URL,
+  API_ROUTE_APARMENT,
+  API_ROUTE_CONTRACT,
+} from "../utils/constants";
+import { routes } from "react-micro-router";
+
+//
+//  REQUEST ACTION ROUTES
+//
+
+const ADD = "/add";
+const DELETE = "/delete/";
+const IMPORT = "/import";
+const SEARCH = "/search";
+const FORM_HEADER = {
+  "Content-Type": "multipart/form-data",
+};
 
 export const baseURL = axios.create({ baseURL: TEST_URL });
+
+//
+//  BASE REQUEST TEMPLATES
+//
+
+const baseRequestAddAPI = async (route, data) =>
+  baseURL.post(route + ADD, data).then((response) => response.status);
+
+const baseRequestDeleteAPI = async (route, id) =>
+  baseURL.post(route + DELETE + id).then((response) => response.status);
+
+const baseRequestSearchAPI = async (route, query, page = 0, pageSize = 10) =>
+  baseURL.get(route + SEARCH, {
+    params: {
+      q: query,
+      page: page,
+      pageSize: pageSize,
+    },
+  });
+
+const baseRequestImportFileAPI = async (route, formData) =>
+  baseURL
+    .post(route + IMPORT, formData, { headers: FORM_HEADER })
+    .then((response) => response.status);
+
+export const exportFileAPI = (route, getTemplate = false) => {
+  return TEST_URL + route + "/export?getTemplate=" + getTemplate;
+};
+
+//
+// APARTMENT REQUESTS
+//
 
 export const fetchApartmentsAPI = async (page, pageSize) =>
   baseURL
@@ -29,22 +78,23 @@ export const fetchApartmentsAPI = async (page, pageSize) =>
 export const importApartmentsAPI = async (formData) => {
   console.log("SUBMIT FILES");
   console.log(formData);
-  baseURL
-    .post(API_ROUTE_APARMENT + "/import", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    })
-    .then((response) => console.log(response))
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    });
+  baseRequestImportFileAPI(API_ROUTE_APARMENT, formData).catch((error) =>
+    console.log(error)
+  );
 };
 
-export const exportFileAPI = (route, getTemplate = false) => {
-  return TEST_URL + route + "/export?getTemplate=" + getTemplate;
-};
+export const addAparmentAPI = async (apartmentDTO) =>
+  baseURL.post(API_ROUTE_APARMENT + ADD, apartmentDTO);
+
+export const deleteApartmentAPI = async (id) =>
+  baseRequestDeleteAPI(API_ROUTE_APARMENT, id);
+
+//
+// APARTMENT REQUESTS
+//
+
+export const deleteContractAPI = async (id) =>
+  baseRequestDeleteAPI(API_ROUTE_CONTRACT, id);
 
 export const fetchTestApartment = testApartmentsOnly;
 export const fetchTestContract = testContractsOnly;
