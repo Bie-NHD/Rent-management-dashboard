8d3c439bbab44e9363878ab32944c7e6418ba686
Duyên Ng <huongduyen.work@gmail.com>
2024-04-17 22:24:54 +0700

    feat: add material-react-table to ApartmentListPage

--- src/pages/ApartmentListPage.jsx
+++ src/pages/ApartmentListPage.jsx
@@ -1,77 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import BottomPaginationBar from "../components/BottomPaginationBar";
-import {
-  API_ROUTE_APARMENT,
-  PAGE_SIZE_OPTIONS,
-  STATUS_OK,
-} from "../utils/constants";
+import { API_ROUTE_APARMENT, STATUS_OK } from "../utils/constants";
 import PageHeader from "../components/PageHeader";
 import ImportButton from "../components/buttons/ImportButton";
 import ExportButton from "../components/buttons/ExportButton";
 import { APARTMENT_API as api } from "../api/apartment";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-import PlaceHolder from "../components/placeholder/PlaceHolder";
-import { Paper, Skeleton } from "@mui/material";
 import CustomReusableDialog from "../components/CustomReusableDialog";
-import ErrorPlaceHolder from "../components/placeholder/ErrorPlaceHolder";
-import ApartmentList from "../sections/apartment-page/ApartmentList";
 import toast from "react-hot-toast";
 import { useModal } from "@ebay/nice-modal-react";
 import NewApartmentDialog from "../sections/apartment-page/NewApartmentDialog";
+import ApartmentTable from "../sections/apartment-page/ApartmentTable";
 
 // ---------------------------------------------------------------------
 
-// interface Pagination{
-//   page: number,
-//   totalPages: number,
-//   pageSize: number
-// }
-
-//  type Action = {type: "change_page"} || {type: "change_page_size"}
-
 const Actions = {
-  CHANGE_PAGE: "change_page",
-  CHANGE_PAGE_SIZE: "change_page_size",
   CREATE_ITEM: "create_item",
   UPDATE_ITEM: "update_item",
-};
-
-const paginationReducer = (state, action) => {
-  switch (action.type) {
-    case Actions.CHANGE_PAGE: {
-      return {
-        ...state,
-        currPage: action.nextPage,
-      };
-    }
-    case Actions.CHANGE_PAGE_SIZE: {
-      return {
-        ...state,
-        // change state to new pageSize
-        pageSize: action.pageSize,
-        currPage: state.currPage >= action.totalPages ? 0 : state.currPage,
-      };
-    }
-  }
-  throw Error("Unknown action: " + action.type);
 };
 
 //
 // fetch data
 //
-
-async function fetchApartments(currPage, pageSize, setApartments = () => {}) {
-  api.fetch(currPage, pageSize).then((data) => {
-    // setApartments here
-    setApartments(data.apartments);
-    // return data so the paginationReducer could use
-    return data;
-  });
-}
 
 const successToast = () => toast.success("Successfully updated!");
 const errorToast = (message) => toast.error(message);
@@ -81,91 +33,12 @@ const errorToast = (message) => toast.error(message);
  * COMPONENT AllApartmentsPage
  */
 const ApartmentListPage = () => {
-  const [apartments, setApartments] = React.useState([]);
-  const [openFormDialog, setOpenFormDialog] = useState(false);
   const [openAlertDialog, setOpenAlertDialog] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [alertDialog, setAlertDialog] = useState(null);
-  const [dialogContent, setDialogContent] = useState(null);
-  const [paginationState, setPaginationState] = useState({
-    currPage: 0,
-    totalPages: 1,
-    pageSize: PAGE_SIZE_OPTIONS[1],
-  });
   const [itemToDelete, setItemToDelete] = useState(null);
   const [itemToUpdate, setItemToUpdate] = useState(null);
   const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
   //
   const newApartmentModal = useModal(NewApartmentDialog);
-
-  React.useEffect(() => {
-    setIsLoading(true);
-    _loadApartments();
-  }, [paginationState.currPage, paginationState.pageSize]);
-
-  function _loadApartments() {
-    setError(null);
-    api
-      .fetch(paginationState.currPage, paginationState.pageSize)
-      .then((data) => {
-        const pageInfo = data.page;
-        const apartmentsInfo = data.apartments;
-
-        setApartments(apartmentsInfo);
-        console.log("APARTMENTS LOADED");
-
-        const newCurrPage =
-          pageInfo.totalPages <= paginationState.currPage
-            ? 0
-            : paginationState.currPage;
-
-        const newTotalPages =
-          pageInfo.totalPages != paginationState.totalPages
-            ? pageInfo.totalPages
-            : paginationState.totalPages;
-
-        setPaginationState((prevState) => {
-          return {
-            ...prevState,
-            totalPages: newTotalPages,
-            currPage: newCurrPage,
-          };
-        });
-
-        setIsLoading(false);
-
-        console.log(
-          `USEEFFECT: CURRPAGE ${paginationState.currPage} | TOTALPAGE ${paginationState.totalPages} | PageInfoTotal ${pageInfo.totalPages}\n
-          PAGESIZE: ${paginationState.pageSize}`
-        );
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }
-
-  function _onPaginationChange(event, page) {
-    //
-    // page index at 1, while currPage index at 0
-    //
-
-    setPaginationState((prevState) => {
-      return {
-        ...prevState,
-        currPage: page - 1,
-      };
-    });
-  }
-
-  function _onPageSizeChange(event) {
-    setPaginationState((prevState) => {
-      return {
-        ...prevState,
-        pageSize: event.target.value,
-      };
-    });
-  }
 
   function _handleCloseUpdateFormDialog() {
     setOpenUpdateDialog(false);
@@ -207,36 +80,17 @@ const ApartmentListPage = () => {
     setOpenFormDialog(false);
   }
 
-  function _handleCloseSnackBar() {
-    // setOpenSuccessSnackBar(false);
-  }
-
   function _handleDeleteItem() {
     //
     // if itemToDelete != null
     //
     api.delete(itemToDelete.id).then((res) => {
       if (STATUS_OK.indexOf(res.status) != -1) {
-        // setOpenSuccessSnackBar(true);
         _loadApartments();
         setItemToDelete(null);
         successToast();
       }
     });
-  }
-
-  function _onClickButtonDelete(item) {
-    console.log("DEL BTN CLICKED");
-
-    // Open Alert Dialog
-    console.log("HANDLING WN DLG");
-    setOpenAlertDialog(true);
-    setItemToDelete(item);
-  }
-
-  function _onClickButtonUpdate(item) {
-    setItemToUpdate(item);
-    setOpenUpdateDialog(true);
   }
 
   function _handleOKDeleteWarningDialog() {
@@ -249,18 +103,6 @@ const ApartmentListPage = () => {
     setOpenAlertDialog(false);
     setItemToDelete(null);
   }
-
-  const NewApartmentFormDialog = () => (
-    <CustomReusableDialog
-      open={openFormDialog}
-      handleClose={() => setOpenFormDialog(false)}
-      onSubmit={(event) => _handleSubmitForm(event, "create")}
-      dialogContent={<NewApartmentFormContent />}
-      title="New Apartment"
-      dialogType="form"
-      okText="Submit"
-    />
-  );
 
   const UpdateApartmentFormDialog = () => (
     <CustomReusableDialog
@@ -359,9 +201,6 @@ const ApartmentListPage = () => {
       <UpdateApartmentFormDialog />
       <DeleteWarningDialog />
       <PageHeader>Apartments</PageHeader>
-      {/* {isLoading ? ( */}
-      {/* <Skeleton animation="wave" height={120} />
-      ) : ( */}
       <Container
         sx={{
           display: "flex",
@@ -372,93 +211,17 @@ const ApartmentListPage = () => {
         <Button
           variant="contained"
           startIcon={<AddCircleIcon />}
-          onClick={
-            // () => setOpenFormDialog(true)
-            () => newApartmentModal.show()
-          }
+          onClick={() => newApartmentModal.show()}
         >
           New
         </Button>
         <ImportButton importType={API_ROUTE_APARMENT} />
         <ExportButton exportType={API_ROUTE_APARMENT} />
       </Container>
-      {/* // )} */}
-
-      {
-        //
-        //  apartments != null && apartments.length > 0
-        //
-        apartments && apartments.length && !isLoading ? (
-          <ApartmentList
-            apartments={apartments}
-            handleDelete={_onClickButtonDelete}
-            handleUpdate={_onClickButtonUpdate}
-          />
-        ) : error ? (
-          <ErrorPlaceHolder onClick={_loadApartments} />
-        ) : (
-          <PlaceHolder />
-        )
-      }
-
-      <BottomPaginationBar
-        // page={currPage + 1}
-        page={paginationState.currPage + 1}
-        pageSizeOptions={PAGE_SIZE_OPTIONS}
-        totalPages={paginationState.totalPages}
-        onPaginationChange={_onPaginationChange}
-        onPageSizeChange={_onPageSizeChange}
-      />
+      <ApartmentTable />
     </>
   );
 };
 
 // EXPORT
 export default ApartmentListPage;
-
-const NewApartmentFormContent = () => (
-  <>
-    <TextField
-      autoFocus
-      required
-      margin="dense"
-      id="txt-address"
-      name="address"
-      label="Address"
-      type="text"
-      fullWidth
-      variant="outlined"
-    />
-    <TextField
-      autoFocus
-      required
-      margin="dense"
-      id="txt-retailPrice"
-      name="retailPrice"
-      label="Retail Price"
-      type="number"
-      fullWidth
-      variant="outlined"
-      defaultValue={"100000"}
-      inputProps={{
-        min: "100000",
-        step: "10000",
-      }}
-    />
-    <TextField
-      autoFocus
-      required
-      margin="dense"
-      id="txt-numberOfRoom"
-      name="numberOfRoom"
-      label="Number of rooms"
-      type="number"
-      fullWidth
-      variant="outlined"
-      defaultValue={"1"}
-      inputProps={{
-        min: "1",
-      }}
-    />
-  </>
-);
--- src/sections/apartment-page/ApartmentTable.tsx
+++ src/sections/apartment-page/ApartmentTable.tsx
@@ -1,13 +1,130 @@
-import { MaterialReactTable, useMaterialReactTable } from "material-react-table";
+import {
+  MRT_ColumnDef,
+  MRT_PaginationState,
+  MRT_SortingState,
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
+import { useMemo, useState } from "react";
+import { Stack, Skeleton } from "@mui/material";
+import { useApartments } from "../../hooks";
+import Apartment from "../../models/Apartment";
+import { IconButton, Tooltip } from "@mui/material";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
-import React from "react";
+// ------------------------------------
 
-type Props = {
-  data: Array<TApartment>;
-};
+const Loading = () => (
+  <Stack spacing={2} sx={{ minHeight: "100", height: "40%", marginY: "2rem" }}>
+    <Skeleton
+      variant="rectangular"
+      animation="wave"
+      height={"20%"}
+      sx={{ minHeight: 30 }}
+    />
+    <Skeleton
+      variant="rectangular"
+      animation="wave"
+      height={"70%"}
+      sx={{ minHeight: 150 }}
+    />
+    <Skeleton
+      variant="rectangular"
+      animation="wave"
+      height={"70%"}
+      sx={{ minHeight: 150 }}
+    />
+    <Skeleton
+      variant="rectangular"
+      animation="wave"
+      height={"70%"}
+      sx={{ minHeight: 150 }}
+    />
+  </Stack>
+);
 
-const ApartmentTable = (props: Props) => {
-  return <div>ApartmentTable</div>;
+// ------------------------------------
+
+const ApartmentTable = () => {
+  const [pagination, setPagination] = useState<MRT_PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const [sorting, setSorting] = useState<MRT_SortingState>([]);
+
+  /**
+   *  Data fetching
+   */
+  const {
+    isLoading,
+    isError,
+    isRefetching,
+    data: { data = [], meta } = {},
+    error,
+    refetch,
+  } = useApartments({
+    variables: {
+      page: pagination.pageIndex, //refetch when pagination.pageIndex changes
+      pageSize: pagination.pageSize, //refetch when pagination.pageSize changes
+      sortBy: sorting?.map((value) => value.id).toString(), //refetch when sorting changes
+    },
+  });
+
+  // Define columns ---------------------------------------
+  const columns = useMemo<MRT_ColumnDef<Apartment>[]>(
+    () => [
+      {
+        accessorKey: "address",
+        header: "Address",
+      },
+      {
+        accessorKey: "numberOfRoom",
+        header: "Number Of Room",
+      },
+      {
+        accessorKey: "retailPrice",
+        header: "Retail Price",
+      },
+    ],
+    []
+  );
+
+  // Define table -----------------------------------------
+
+  const table = useMaterialReactTable({
+    columns: columns,
+    data: data,
+    rowCount: meta?.totalRowCount ?? 0,
+    manualPagination: true, //turn off built-in client-side pagination
+    manualSorting: true, //turn off built-in client-side sorting
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+    state: {
+      isLoading,
+      pagination,
+      showAlertBanner: isError,
+      showProgressBars: isLoading || isRefetching,
+      sorting,
+    },
+    renderTopToolbarCustomActions: () => (
+      <Tooltip arrow title="Refresh Data">
+        <IconButton onClick={() => refetch()}>
+          <RefreshIcon />
+        </IconButton>
+      </Tooltip>
+    ),
+  });
+
+  // -------------------------------------------------------
+
+  // Show loading
+  if (isLoading) return <Loading />;
+  // Show, throw Error
+  if (isError) throw error || new Error("Error with ApartmentTable");
+
+  // -------------------------------------------------------
+
+  return <MaterialReactTable table={table} />;
 };
 
 export default ApartmentTable;
