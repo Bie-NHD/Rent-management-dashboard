253d4d00cc6bd1a3149f72bbf05ce13b9e405ef3
Duyên Ng <huongduyen.work@gmail.com>
2024-04-17 22:25:29 +0700

    chore: update imports

--- src/pages/AllContractsPage.jsx
+++ src/pages/AllContractsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { fetchTestContract } from "../api";
+import { fetchTestContract } from "../api/apartment";
 import { formatDate, formatId } from "../utils/stringFormats";
 import {
   TableContainer,
--- src/pages/AllCustomersPage.jsx
+++ src/pages/AllCustomersPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { APARTMENT_API as api } from "../api";
+import { APARTMENT_API as api } from "../api/apartment";
 import { TableCell } from "@mui/material";
 import { formatId } from "../utils/stringFormats";
 
