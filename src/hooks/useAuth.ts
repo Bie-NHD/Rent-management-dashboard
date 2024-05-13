f536cccd43190d9145baba19e7ed1a4d268203e9
Ng Huong Duyen <huongduyen.work@gmail.com>
2024-05-13 14:01:23 +0700

    update useAuth types

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,4 @@
 import { useContext } from "react";
 import AuthContext from "../context/AuthProvider";
-const useAuth = (): IUseAuthHookResult => useContext(AuthContext);
+const useAuth = (): IUseAuthHookResult | null => useContext(AuthContext);
 export default useAuth;
