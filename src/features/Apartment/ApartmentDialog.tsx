5ff2a27cba3f21472e9e4f67c2f638a149b9f895
Ng Huong Duyen <huongduyen.work@gmail.com>
2024-05-14 10:33:45 +0700

    refactor ApartmentDialog

--- src/features/Apartment/ApartmentDialog.tsx
+++ src/features/Apartment/ApartmentDialog.tsx
@@ -10,84 +10,45 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
-import { OutlinedTextFieldProps } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import {
-  useForm,
-  useController,
-  SubmitHandler,
-  Control,
-} from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import NiceModal, { useModal } from "@ebay/nice-modal-react";
 import { yupResolver } from "@hookform/resolvers/yup";
-import ApartmentSchema from "../../types/ApartmentSchema";
+import RHFOutlinedTextField from "../../components/RHFTextField";
+import { ObjectSchema, object, string, number } from "yup";
 
-interface ApartmentInputProps extends OutlinedTextFieldProps {
-  /**
-   * should match the name of keys of type ```Apartment```
-   */
-  name: "address" | "numberOfRoom" | "retailPrice";
-  /**
-   * to define ```Control``` element created by ```React-Hook-Form```'s ```useForm``` Hook
-   */
-  control: Control<Omit<Apartment, "id">, any>;
-  /**
-   * to define ```step``` HTMLAttribute for ```input["number"]```
-   */
-}
+type ApartmentInputs = Omit<Apartment, "id">;
 
-const Input: React.FC<ApartmentInputProps> = ({
-  name,
-  control,
-  ...otherProps
-}: ApartmentInputProps) => {
-  // Hook rpovided by React-Hook-Form
-  const {
-    field, // { name, value, onChange, onBlur }
-    fieldState, // {error}
-  } = useController({
-    name,
-    control,
-  });
-  return (
-    <TextField
-      {...field}
-      {...otherProps}
-      fullWidth
-      helperText={fieldState?.error?.message || ""}
-      error={!!fieldState.error} // !! is a double boolean operator, convert to Boolean type
-      id={`txt-${field.name}`}
-      margin="dense"
-      // ref={field.ref}
-    />
-  );
-};
+const schema: ObjectSchema<ApartmentInputs> = object({
+  address: string()
+    .label("Address")
+    .required("Address is required")
+    .default(""),
+  numberOfRoom: number().label("Number of Rooms").required().min(1).default(1),
+  retailPrice: number()
+    .label("Retail Price")
+    .required()
+    .min(100000)
+    .default(100000),
+});
 
 const ApartmentDialog = NiceModal.create(
   ({ apartment }: { apartment: Apartment }) => {
-    /**
-     * Hook provided by Nice-modal-react
-     */
+    // Hook provided by Nice-modal-react
     const modal = useModal();
 
-    const defaultValues = useMemo<Omit<Apartment, "id">>(() => {
+    const defaultValues = useMemo<ApartmentInputs>(() => {
       if (apartment) {
         const { id, ...others } = apartment;
         return others;
-      } else return ApartmentSchema.getDefault();
+      } else return schema.getDefault();
     }, []);
 
-    const {
-      handleSubmit,
-      control,
-      reset,
-      formState: { errors },
-    } = useForm<Omit<Apartment, "id">, any>({
+    const { handleSubmit, control } = useForm<ApartmentInputs>({
       defaultValues: defaultValues,
-      resolver: yupResolver<Omit<Apartment, "id">>(ApartmentSchema),
+      resolver: yupResolver<ApartmentInputs>(schema),
     });
 
-    const onSubmitNew: SubmitHandler<Omit<Apartment, "id">> = (
+    const onSubmitNew: SubmitHandler<ApartmentInputs> = (
       data, //TFieldValues
       event
     ) => {
@@ -107,14 +68,13 @@ const ApartmentDialog = NiceModal.create(
       >
         <DialogTitle>New Apartment</DialogTitle>
         <DialogContent>
-          <Input
+          <RHFOutlinedTextField
             variant="outlined"
             name="address"
             label="Address"
-            type="text"
             control={control}
           />
-          <Input
+          <RHFOutlinedTextField
             variant="outlined"
             name="retailPrice"
             label="Retail Price"
@@ -122,7 +82,7 @@ const ApartmentDialog = NiceModal.create(
             control={control}
             inputProps={{ step: 1000, format: "###.###.###" }}
           />
-          <Input
+          <RHFOutlinedTextField
             variant="outlined"
             name="numberOfRoom"
             label="Number of rooms"
@@ -133,7 +93,7 @@ const ApartmentDialog = NiceModal.create(
         </DialogContent>
         <DialogActions>
           <Button onClick={modal.remove}>Cancel</Button>
-          <Button type="submit">Submit</Button>
+          <Button type="submit">{apartment ? "Save" : "Create"}</Button>
         </DialogActions>
       </Dialog>
     );
