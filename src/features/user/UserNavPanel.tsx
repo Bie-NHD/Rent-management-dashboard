daea82326113ae67dd6167567ffb6b68baab14a3
Duyên Ng <huongduyen.work@gmail.com>
2024-05-18 13:05:16 +0700

    fix: not logging out at ```UserNavPanel``` & feat: auto logging out when refresh_token is unavailable

--- src/features/user/UserNavPanel.tsx
+++ src/features/user/UserNavPanel.tsx
@@ -6,10 +6,18 @@ import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import EditIcon from "@mui/icons-material/Edit";
 import NiceModal from "@ebay/nice-modal-react";
 import UserUpdateDialog from "./UserUpdateDialog";
+import { useNavigate } from "react-router-dom";
 
 const UserNavPanel = () => {
   const { logout } = useAuth();
   const { user } = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout().finally(() => {
+      navigate("/login", { replace: true });
+    });
+  };
 
   const handleOnClickEdit = () => {
     NiceModal.show(UserUpdateDialog, {
@@ -35,10 +43,10 @@ const UserNavPanel = () => {
       <Typography align="center">{user.fullName}</Typography>
       <Typography align="center">{`@${user.username}`}</Typography>
       <Typography align="center">{user?.role}</Typography>
-      <Button startIcon={<EditIcon />} onClick={() => handleOnClickEdit()}>
+      <Button startIcon={<EditIcon />} onClick={handleOnClickEdit}>
         Edit
       </Button>
-      <Button variant="outlined" startIcon={<ExitToAppIcon />} onClick={() => logout()}>
+      <Button variant="outlined" startIcon={<ExitToAppIcon />} onClick={handleLogout}>
         Logout
       </Button>
     </Container>
