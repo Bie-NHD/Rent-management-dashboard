955c7a9377b3bed2165a374f7960d1012edf7b26
Duyên Ng <huongduyen.work@gmail.com>
2024-05-22 07:42:59 +0700

    add ChangePassword validations

--- src/features/Login/ChangePassword.tsx
+++ src/features/Login/ChangePassword.tsx
@@ -1,36 +1,79 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import React from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import { ObjectSchema, object, string } from "yup";
+import { ObjectSchema, object, ref, string } from "yup";
 import { ChangePasswordProps } from "../../types/props";
 import LoginLayout from "../../components/LoginLayout";
 import { Box, Button, Container, Stack } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
 import useUser from "../../hooks/useUser";
 import ToggleablePasswordTextField from "../../components/inputs/ToggleablePasswordTextField";
+import { Label } from "@mui/icons-material";
+import { Api } from "../../api";
+import toast from "react-hot-toast";
 
-// TODO: Add validation
-const schema: ObjectSchema<ChangePasswordProps> = object({
-  currentPassword: string().required().default(""),
-  newPassword: string().required(),
-  repeatNewPassword: string().required(),
+const schema: ObjectSchema<ChangePasswordProps> = object().shape({
+  currentPassword: string().required().default("").label("Current password"),
+  newPassword: string()
+    .label("New password")
+    .ensure()
+    .when("currentPassword", {
+      is: (value: string) => value != "",
+      then: (schema) =>
+        schema
+          .required()
+          .test(
+            "newPassword_check",
+            "New password must be different from current password",
+            function (value: string, context) {
+              return context.parent["currentPassword"] != value;
+            }
+          ),
+    }),
+  repeatNewPassword: string()
+    .label("Repeat password")
+    .ensure()
+    .when("newPassword", {
+      is: (value: string) => value != "",
+      then: (schema) =>
+        schema.required().oneOf([ref("newPassword")], ({ label }) => `${label} doesn't match.`),
+    }),
 });
 const ChangePassword = () => {
   const { user } = useUser();
   const location = useLocation();
   const from = location?.state?.from || "/";
   const navigate = useNavigate();
-
+  const [_errors, setErrors] = useState("");
   const { handleSubmit, control } = useForm<ChangePasswordProps>({
     resolver: yupResolver(schema),
   });
 
-  // TODO: Handle submit
-  const onSubmit: SubmitHandler<ChangePasswordProps> = async (data) => {};
+  const onSubmit: SubmitHandler<ChangePasswordProps> = async (data) => {
+    const resp = await Api.instance
+      .post<ApiResponse>(`/users/changePassword/${user?.id}`, {
+        currentPassword: data.currentPassword,
+        newPassword: data.newPassword,
+      })
+      .then((res) => res.data);
+
+    if (resp.statusCode == 200) {
+      toast.success(resp.message);
+      navigate(from);
+    } else {
+      console.log(resp.statusCode, resp.message);
+      setErrors(resp.message);
+    }
+  };
 
   return (
     <LoginLayout title={"Change password"}>
       <Container component={"form"} onSubmit={handleSubmit(onSubmit)}>
+        {!!_errors && (
+          <Box marginBlock={2} padding={2} sx={{ border: "solid red 1px", color: "red" }}>
+            {_errors}
+          </Box>
+        )}
         <Stack spacing={2}>
           <Controller
             name={"currentPassword"}
@@ -55,7 +98,9 @@ const ChangePassword = () => {
                 {...field}
                 label="New Password"
                 error={!!fieldState.error}
-                helperText={!!fieldState.error && fieldState.error.message}
+                helperText={
+                  (!!fieldState.error && fieldState.error.message) || "Your new password here"
+                }
                 fullWidth
                 onChange={(e) => field.onChange(e.target.value)}
                 margin="dense"
